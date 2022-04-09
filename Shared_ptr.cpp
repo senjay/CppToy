d9af07a21e51935d37af4778fcead1bf0af3aa0a
senjay <760832791@qq.com>
2022-04-09 13:55:06 +0800

    引用计数线程安全

--- Shared_ptr.cpp
+++ Shared_ptr.cpp
@@ -1,4 +1,8 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<thread>
+#include<chrono>
+#include<vector>
+#include<atomic>
 using namespace std;
 /*
 构造函数
@@ -12,23 +16,52 @@ operator*
 再定义一个std::mutex * mutex,构造函数初始化的时候 再mutex =new mtuex()
 每次对计数加减时 加锁
 同时所有ptr要共享这个mutex
+或者如下使用atomic
 */
+class ReferenceCnt
+{
+    public:
+        ReferenceCnt()
+        {
+            cnt_.store(1);
+        }
+        void addOne()
+        {
+            cnt_++;
+        }
+        void subOne()
+        {
+            cnt_.fetch_sub(1);
+        }
+        bool iszero()
+        {
+            int expect=0;
+            return cnt_.compare_exchange_strong(expect,0);
+        }
+        int value()
+        {
+            return cnt_.load();
+        }
+    private:
+    std::atomic<int> cnt_;
+};
 template <typename T>
 class Shared_ptr{
-public: 
+public:
+    Shared_ptr():ptr_(nullptr),refCnt_(nullptr){};
     Shared_ptr(T*p)
     {
         ptr_=p;
-        cnt_=new int(1);
+        refCnt_=new ReferenceCnt();
         std::cout<<"Call Constructor"<<endl;
     }
 
     //拷贝构造 记得cnt+1
     Shared_ptr(const Shared_ptr<T>& S)
     {
-        cnt_=S.cnt_;
         ptr_=S.ptr_;
-        (*cnt_)++;
+        refCnt_=S.refCnt_;
+        refCnt_->addOne();
         std::cout<<"Call copy constructor"<<endl;
     }
     /*
@@ -40,17 +73,17 @@ public:
     {
         //必须得先等号右边的cnt+1 否则如果遇到这种情况 P1=P1，或把P1销毁,
         //虽然r是const 但改变的是r的成员的指针指向的值
-        (*(r.cnt_))++;
-        if(!ptr_)
+        r.refCnt_->addOne();
+        if(ptr_!=nullptr)
         {
-            (*cnt_)--;
-            if((*cnt_)==0)
+            refCnt_->subOne();
+            if(refCnt_->iszero())
             {
                 delete ptr_;
-                delete cnt_;
+                delete refCnt_;
             }
         }
-        cnt_=r.cnt_;
+        refCnt_=r.refCnt_;
         ptr_=r.ptr_;
         std::cout<<"Call operator= "<<endl;
         return *this;
@@ -58,10 +91,11 @@ public:
     //析构 cnt-1 ,如果cnt==0 则delete
     ~Shared_ptr()
     {
-        (*cnt_)--;
-        if((*cnt_)==0)
+        if(ptr_==nullptr)return;
+        refCnt_->subOne();
+        if(refCnt_->iszero())
         {
-            delete cnt_;
+            delete refCnt_;
             delete ptr_;
         }
         std::cout<<"Call Destructor"<<endl;
@@ -75,10 +109,10 @@ public:
     {
         return (*ptr_);
     }
-    int getCnt(){return *(cnt_);};
+    int getCnt(){return refCnt_->value();};
 private:
     T* ptr_;
-    int *cnt_;
+    ReferenceCnt *refCnt_;
 };
 
 
@@ -90,17 +124,40 @@ class Obj{
 
 void PringCnt(Shared_ptr<Obj>&s)
 {
-    cout<<"cnt_:"<<s.getCnt()<<endl;
+    printf("cnt_:%d\n",s.getCnt());
 }
+
+
 int main() {
+
     Shared_ptr<Obj>p1(new Obj(888));
+    std::vector<Shared_ptr<Obj>>L(25);
+    std::thread t1([&](){
+        for(int i=0;i<10;++i)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
+            L[i]=p1;
+            PringCnt(p1);
+        }
+    });
+    std::thread t2([&](){
+        for(int i=0;i<10;++i)
+        {
+              L[i+10]=p1;
+             PringCnt(p1);
+        }
+    });
+    t1.join();
+    t2.join();
+    //
+    std::cout<<"now p1 should have 21,it has "<<p1.getCnt()<<std::endl;
     Shared_ptr<Obj>p2(p1);
     Shared_ptr<Obj>p3=p1;
+    std::cout<<"now p1 should have 23,it has "<<p1.getCnt()<<std::endl;
     Shared_ptr<Obj>p4(new Obj(999));
-    PringCnt(p4);
+    std::cout<<"now p4 should have 1,it has "<<p4.getCnt()<<std::endl;
     p4=p4;
-    PringCnt(p4);
-    PringCnt(p4);
+    std::cout<<"now p4 should have 1,it has "<<p4.getCnt()<<std::endl;   
     cout<<p1->val<<endl;
     cout<<(*p1).val<<endl;
 }
